c03e6d92c35eb93d500a6476ef6ad3b7d216eea5
Alec Hartman <alec@alechartman.com>
2012-05-28 01:38:51 -0400

    updating deploy settings

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+require 'bundler/capistrano'
+set :application, "hg"
+set :repository,  "git@github.com:alechartman/thehartmangallery.git"
+set :user, "railsapps"
+set :use_sudo, false
+set :deploy_to, "/home/railsapps/public_html/#{application}"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "hg.alechartman.com"                          # Your HTTP server, Apache/etc
+role :app, "hg.alechartman.com"                          # This may be the same as your `Web` server
+role :db,  "hg.alechartman.com", :primary => true # This is where Rails migrations will run
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+load 'deploy/assets'
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+require './config/boot'
